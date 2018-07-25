a8ec6d8f5dbb1582baca0e21c084995d22ed824b
carl <137152533@qq.com>
2018-07-25 17:53:59 +0800

    片花定时导出

--- bin/xls_cron.sh
+++ bin/xls_cron.sh
@@ -6,7 +6,9 @@
  xls_id1="cron_vod_$nowtime"
  xls_id2="cron_ad_$nowtime"
  xls_id3="cron_order_$nowtime"
+ xls_id4="cron_trailer_$nowtime"
 
  curl -d "starttime=$starttime&endtime=$endtime&xlstype=1&xls_id=$xls_id1" "http://127.0.0.1:81/xlsm/create_xls.php"
  curl -d "starttime=$starttime&endtime=$endtime&xlstype=2&xls_id=$xls_id2" "http://127.0.0.1:81/xlsm/create_xls.php"
  curl -d "starttime=$starttime&endtime=$endtime&xlstype=3&xls_id=$xls_id3" "http://127.0.0.1:81/xlsm/create_xls.php"
+ curl -d "starttime=$starttime&endtime=$endtime&xlstype=4&xls_id=$xls_id4" "http://127.0.0.1:81/xlsm/create_xls.php"
